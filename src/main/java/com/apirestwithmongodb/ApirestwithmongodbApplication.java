de43acd10d9329cd825ae1c51aed7d40da86d64b
Porto <joao.porto@dextra-sw.com>
2019-08-15 15:01:22 -0300

    First commit

---
+++ src/main/java/com/apirestwithmongodb/ApirestwithmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.apirestwithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestwithmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestwithmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestwithmongodb/ApirestwithmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestwithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestwithmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
